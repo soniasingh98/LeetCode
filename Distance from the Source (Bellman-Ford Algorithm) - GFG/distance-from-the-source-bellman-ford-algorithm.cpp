55e47badae17108c516711e1d6c13cb54cb0b58b
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2022-06-30 15:15:37 +0530

    Added solution - LeetHub

---
+++ Distance from the Source (Bellman-Ford Algorithm) - GFG/distance-from-the-source-bellman-ford-algorithm.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+	public:
+	/*  Function to implement Dijkstra
+    *   adj: vector of vectors which represents the graph
+    *   S: source vertex to start traversing graph with
+    *   V: number of vertices
+    */
+    vector <int> bellman_ford(int V, vector<vector<int>> adj, int S) {
+        // Code here
+      
+        vector<int>dist(V,1e8);
+        dist[S]=0;
+         int u,v,wt;
+        for(int i=1;i<=V-1;i++){
+            for(auto it:adj){
+                u=it[0];
+                v=it[1];
+                wt=it[2];
+                if(dist[u]+wt<dist[v]){
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj;
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1;
+            t1.push_back(u);
+            t1.push_back(v);
+            t1.push_back(w);
+            adj.push_back(t1);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.bellman_ford(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
