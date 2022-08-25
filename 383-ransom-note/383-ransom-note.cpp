56296b398078ade20a5dfd3517a7b1ce99d6c773
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2022-08-25 20:24:48 +0530

    Time: 12 ms (91.71%), Space: 8.8 MB (39.49%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[256]={0};
+        for(int i=0;i<magazine.size();i++){
+            count[magazine[i]]++;
+        }
+        for(int i=0;i<ransomNote.size();i++){
+            if(count[ransomNote[i]]==0){
+                return false;
+            }
+            else{
+                count[ransomNote[i]]--;
+            }
+        }
+        return true;
+    }
+};
