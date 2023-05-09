bdcb803592ab4c3b2bb73bc02108c78d56183280
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2023-05-09 20:39:34 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,22 +8,22 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int f(int i,int w,int wt[],int val[],vector<vector<int>>&dp){
-        if(i==0){
-           if(w>=wt[i]) return val[0];
-           return 0;
-        }
-        if(dp[i][w]!=-1)return dp[i][w];
-        int t=INT_MIN;
-        if(w>=wt[i])t=val[i]+f(i-1,w-wt[i],wt,val,dp);
-        int nt=f(i-1,w,wt,val,dp);
-        return dp[i][w]=max(t,nt);
-    }
     int knapSack(int W, int wt[], int val[], int n) 
     { 
        // Your code here
        vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       return f(n-1,W,wt,val,dp);
+       return f(n-1,W,wt,val,n,dp);
+    }
+    int f(int i,int W,int wt[],int val[],int n,vector<vector<int>>&dp){
+        if(i==0){
+            if(W>=wt[i])return val[i];
+            return 0;
+        }
+        if(dp[i][W]!=-1)return dp[i][W];
+        int t=INT_MIN;
+        if(W>=wt[i])t=val[i]+f(i-1,W-wt[i],wt,val,n,dp);
+        int nt=f(i-1,W,wt,val,n,dp);
+        return dp[i][W]= max(t,nt);
     }
 };
 
