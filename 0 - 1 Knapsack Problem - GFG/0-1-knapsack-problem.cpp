9ab4438cd3f52642afa9c610bb5330d5c36a916a
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2023-06-22 09:44:30 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -10,8 +10,19 @@ class Solution
     //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
     { 
-       vector<vector<int>>dp(n,vector<int>(W+1,-1));
-       return f(n-1,wt,val,W,dp);
+       vector<vector<int>>dp(n,vector<int>(W+1,0));
+       for(int w=0;w<=W;w++){
+           if(w>=wt[0])dp[0][w]=val[0];
+       }
+       for(int i=1;i<n;i++){
+           for(int j=0;j<=W;j++){
+               int t=-1e9;
+               if(wt[i]<=j)t=val[i]+dp[i-1][j-wt[i]];
+               int nt=dp[i-1][j];
+               dp[i][j]=max(t,nt);
+           }
+       }
+       return dp[n-1][W];
     }
     int f(int i,int wt[],int val[],int W,vector<vector<int>>&dp){
         if(i==0){
