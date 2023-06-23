e05eec2fbfa7eb9c0d215ab7492268434cfa81d1
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2023-06-23 09:08:36 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -8,14 +8,15 @@ class Solution{
 	int minOperations(string str1, string str2) 
 	{ 
 	  int n=str1.size(),m=str2.size();
-	  vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+	  vector<int>prev(m+1,0),curr(m+1,0);
 	  for(int i=1;i<=n;i++){
 	      for(int j=1;j<=m;j++){
-	          if(str1[i-1]==str2[j-1])dp[i][j]=1+dp[i-1][j-1];
-	          else dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+	          if(str1[i-1]==str2[j-1])curr[j]=1+prev[j-1];
+	          else curr[j]=max(curr[j-1],prev[j]);
 	      }
+	      prev=curr;
 	  }
-	  return (n+m)-2*dp[n][m];
+	  return (n+m)-2*prev[m];
 	} 
 	int f(string &s,string &t,int i,int j,vector<vector<int>>&dp){
 	    if(i<0 || j<0)return 0;
