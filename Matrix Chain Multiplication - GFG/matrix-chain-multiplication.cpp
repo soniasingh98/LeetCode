6b15902f0c6429daebcf838ade3bbb813a7841cb
soniasingh98 <88448499+soniasingh98@users.noreply.github.com>
2023-06-24 20:29:15 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.cpp
@@ -9,24 +9,22 @@ using namespace std;
 
 class Solution{
 public:
-    int f(int i,int j,int arr[],vector<vector<int>>&dp){
-        if(i==j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int ans=INT_MAX;
-        for(int k=i;k<j;k++){
-            int steps=arr[i-1]*arr[k]*arr[j]+f(i,k,arr,dp)+f(k+1,j,arr,dp);
-            ans=min(steps,ans);
-        }
-        return dp[i][j]= ans;
-    }
-    
     int matrixMultiplication(int N, int arr[])
     {
         // code here
         vector<vector<int>>dp(N,vector<int>(N,-1));
         return f(1,N-1,arr,dp);
     }
-    
+    int f(int i,int j,int arr[],vector<vector<int>>&dp){
+        if(i==j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int mini=INT_MAX;
+        for(int k=i;k<j;k++){
+            int steps=arr[i-1]*arr[k]*arr[j]+f(i,k,arr,dp)+f(k+1,j,arr,dp);
+            mini=min(mini,steps);
+        }
+        return dp[i][j]= mini;
+    }
 };
 
 //{ Driver Code Starts.
